b5e81746764d2ba12f59d8ca41d23c13b98d1dd8
racheldengg <92647291+racheldengg@users.noreply.github.com>
2022-09-22 16:39:04 -0400

    Update Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,54 +1,67 @@
 public class Polynomial {
-    double coefficients[];
+	double coefficients[];
 
-    public Polynomial()
-    {
-	coefficients = new double[1];
-	coefficients[0] = 0;
-    }
-
-    public Polynomial(double listOfCoefficients[])
-    {
-	int n = listOfCoefficients.length;
-	coefficients = new double[n];
-	for(int i = 0; i < n; i++)
+	public Polynomial()
 	{
-	    coefficients[i] = listOfCoefficients[i];
+		coefficients = new double[1];
+		coefficients[0] = 0;
 	}
-    }
 
-    public Polynomial add(Polynomial other)
-    {
-	int n = coefficients.length;
-	for( int i = 0; i<n; i++)
+	public Polynomial(double listOfCoefficients[])
 	{
-	other.coefficients[i] = other.coefficients[i] + coefficients[i];
+		int n = listOfCoefficients.length;
+		coefficients = new double[n];
+		for(int i = 0; i < n; i++)
+		{
+			coefficients[i] = listOfCoefficients[i];
+		}
 	}
-	return other;
-    }
 
-    public double evaluate(double x)
-    {
-        double result = 0;
-        int n = coefficients.length;
-        for(int i = 0; i < n; i++)
-        {
-	    result = result + Math.pow(x, i) * coefficients[i];
+	public Polynomial add(Polynomial other)
+	{
+		if (coefficients.length>other.coefficients.length)
+		{
+			//make a new array of length coefficients.length
+			Polynomial sum = new Polynomial(coefficients);
+			for( int i = 0; i<other.coefficients.length; i++)
+	    	{
+	    		sum.coefficients[i] = other.coefficients[i] + coefficients[i];
+	    	}
+	    	return sum;
+	    }
+	    else
+	    {
+	    	Polynomial sum = new Polynomial(other.coefficients);
+	    	for (int i = 0; i<coefficients.length; i++)
+	    	{
+	    		sum.coefficients[i] = other.coefficients[i] + coefficients[i];
+	    	}
+	    	return sum;
+	    }
 	}
-	    return result;
-    }
 
-    public boolean hasRoot(double x)
-    {
-        double result = evaluate(x);
-        if (result == 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+	public double evaluate(double x)
+	{
+		double result = 0;
+	        int n = coefficients.length;
+	        for(int i = 0; i < n; i++)
+	        {
+	            result = result + Math.pow(x, i) * coefficients[i];
+	        }
+	        return result;
+	    }
+	    
+	    public boolean hasRoot(double x)
+	    {
+	        double result = evaluate(x);
+	        if (result == 0)
+	        {
+	            return true;
+	        }
+	        else
+	        {
+	            return false;
+	        }
+	    }
 
 }
